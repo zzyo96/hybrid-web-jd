3f667e196885363785b181883d2326d27bdb139c
zzyo <1977795940@qq.com>
2020-09-14 16:39:18 +0800

    完成活动组件封装及首页活动的布局，添加活动的json文件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,46 @@
 <template>
   <div class="home">
-    <my-swiper :swiperImgs="swiperImgs.map(item => item.icon)" :height="swiperHeight"></my-swiper>
+    <div class="home-content">
+      <my-swiper :swiperImgs="swiperImgs.map(item => item.icon)" :height="swiperHeight"></my-swiper>
+      <!--520 活动-->
+      <activity>
+        <div class="activity-520">
+          <img v-for="(item,index) in activityDatas" :key="index" :src="item.icon" alt="">
+        </div>
+      </activity>
+    </div>
   </div>
 </template>
 
 <script>
   import MySwiper from '@c/swiper/MySwiper.vue';
   import {axios} from '@js/axios.config.js'
+  import Activity from '@c/currency/Activity.vue'
 
   export default {
     components: {
-      MySwiper
+      MySwiper,
+      Activity
     },
     data() {
       return {
         swiperImgs: [],
-        swiperHeight: '184px'
+        swiperHeight: '184px',
+        activityDatas:[]
       }
     },
     methods: {
       initData: function () {
-        // console.log(this.$http,100)
         axios.get('/swiper.json')
           .then(data => {
-            console.log(data,1233)
             this.swiperImgs = data.list;
+          }).catch(err => {
+          console.log(err);
+        });
+
+        axios.get('/activitys.json')
+          .then(data => {
+            this.activityDatas = data.list;
           }).catch(err => {
           console.log(err);
         });
@@ -38,12 +54,24 @@
 </script>
 
 <style lang="scss" scoped>
+@import '@css/style.scss';
   .home {
     width: 100%;
     height: 100%;
+    background-color: $bgColor;
 
     &-content {
       height: 100%;
+
+      .activity-520{
+        margin-top: px2rem(-8);
+        border-top-left-radius: px2rem(8);
+        border-top-right-radius: px2rem(8);
+
+        img{
+          width: 33.3%;
+        }
+      }
     }
   }
 </style>
---
+++ src/components/currency/Activity.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="activity">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Activity.vue"
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '@css/style.scss';
+
+  .activity {
+    max-height: px2rem(160);
+    position: relative;
+    z-index: 2;
+  }
+
+</style>
