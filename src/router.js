e6f27fcb5d2f40cc0586f6d769a602a67a332934
zzyo <1977795940@qq.com>
2020-09-18 10:54:27 +0800

    完成商品列表的基本结构，及路由跳转

--- src/router.js
+++ src/router.js
@@ -12,6 +12,16 @@ export default new Router({
       path:'/',
       name:'home',
       component:Main
-    }
+    },
+    {
+      path: '/goodsList',
+      name: 'goodsList',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // vue-router 懒加载 - > GoodsList
+      // chunkName -> GoodsList.[hash].js
+      component: () => import('./views/GoodsList.vue')
+    },
   ]
 })
