a33db36cf5cbc09e6d8561749ed95b1319602bef
zzyo <1977795940@qq.com>
2020-09-18 10:33:34 +0800

    完成search组件， 及顶部navigationbar组件，可让顶部随着页面滚动进行不同透明度的展示

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="home">
+  <div class="home" @scroll="onScrollChange" ref="home">
+    <navigation-bar :isShowBack="false" :navBarStyle="navBarStyle">
+      <!-- 左侧插槽 -->
+      <template v-slot:nav-left>
+        <img :src="navBarCurrentSlotStyle.leftIcon" alt="">
+      </template>
+      <!-- 中间插槽 -->
+      <template v-slot:nav-center>
+        <search :bgColor="navBarCurrentSlotStyle.search.bgColor"
+                :hintColor="navBarCurrentSlotStyle.search.hintColor"
+                :icon="navBarCurrentSlotStyle.search.icon">
+        </search>
+      </template>
+      <!-- 右侧插槽 -->
+      <template v-slot:nav-right>
+        <img :src="navBarCurrentSlotStyle.rightIcon" alt="">
+      </template>
+    </navigation-bar>
     <div class="home-content">
       <my-swiper :swiperImgs="swiperImgs.map(item => item.icon)" :height="swiperHeight"></my-swiper>
       <!--520 活动-->
@@ -31,6 +48,8 @@
   import Seconds from '@c/seconds/Seconds.vue'
   import ModeOptions from '@c/currency/ModeOptions.vue'
   import Goods from '@c/goods/Goods.vue';
+  import NavigationBar from '@c/currency/NavigationBar.vue';
+  import Search from '@c/currency/Search.vue';
 
   export default {
     components: {
@@ -38,14 +57,53 @@
       Activity,
       ModeOptions,
       Seconds,
-      Goods
+      Goods,
+      NavigationBar,
+      Search
     },
     data() {
       return {
         swiperImgs: [],
         swiperHeight: '184px',
         activityDatas:[],
-        secondsDatas:[]
+        secondsDatas:[],
+        navBarStyle: {
+          position: 'fixed',
+          backgroundColor: ''
+        },
+        navBarSlotStyle: {
+          // 默认样式
+          normal: {
+            // 左侧插槽
+            leftIcon: require('@img/more-white.svg'),
+            // 中间插槽
+            search: {
+              bgColor: '#ffffff',
+              hintColor: '#999999',
+              icon: require('@img/search.svg')
+            },
+            // 右侧插槽
+            rightIcon: require('@img/message-white.svg')
+          },
+          // 高亮样式
+          highlight: {
+            // 左侧插槽
+            leftIcon: require('@img/more.svg'),
+            // 中间插槽
+            search: {
+              bgColor: '#d7d7d7',
+              hintColor: '#ffffff',
+              icon: require('@img/search-white.svg')
+            },
+            // 右侧插槽
+            rightIcon: require('@img/message.svg')
+          },
+        },
+        navBarCurrentSlotStyle:{},
+        // 记录页面滚动值
+        scrollTopValue: -1,
+        // 锚点值
+        ANCHOR_SCROLL_TOP: 160
       }
     },
     methods: {
@@ -59,10 +117,34 @@
           this.activityDatas = datas[1].list;
           this.secondsDatas = datas[2].list;
         })
+      },
+      /**
+       * 监听页面滚动
+       * 1、获取到当前页面滚动的距离
+       * 2、计算 navBar 背景颜色（navBar 背景透明度）
+       *      当前滚动的距离 / 锚点值 = navBar 背景透明度  opacity
+       * 3、opacity >= 1 ， 当前滚动的距离 已经等于或者超过了 锚点值，当前 navBar 插槽的样式变为 高亮状态的样式。
+       *  否则的话 opacity < 1 ,当前 navBar 插槽的样式，为 默认状态的样式
+       */
+      onScrollChange: function ($event) {
+        // 获取到当前页面滚动的距离
+        this.scrollTopValue = $event.target.scrollTop;
+        // 计算 navBar 背景颜色（navBar 背景透明度）
+        let opacity = this.scrollTopValue / this.ANCHOR_SCROLL_TOP;
+        // 指定 narBar 插槽样式
+        if (opacity >= 1) {
+          this.navBarCurrentSlotStyle = this.navBarSlotStyle.highlight;
+        } else {
+          this.navBarCurrentSlotStyle = this.navBarSlotStyle.normal;
+        }
+
+        // 根据透明比例来设置 navBar 的背景颜色
+        this.navBarStyle.backgroundColor = 'rgba(255, 255, 255, ' + opacity + ')'
       }
     },
-    mounted() {
-      this.initData()
+    created() {
+      this.navBarCurrentSlotStyle = this.navBarSlotStyle.normal;
+      this.initData();
     }
   }
 
---
+++ src/components/currency/NavigationBar.vue
@@ -0,0 +1,104 @@
+<template>
+    <!-- 
+        能力：
+            1、默认的展示效果 -> 左边后退按钮的图标、中间页面的名称、右边是空白的内容
+            2、可通过插槽来配置具体的展示样式 -> 左、中、右 三个插槽，父组件可以通过这三个插槽来制定对应位置的布局。
+            3、可以接收从外部指定的一个样式（可以在父组件中指定 navigationBar 的 style）。
+     -->
+    <div class="nav-bar z-index-max" :style="navBarStyle"
+     :class="[{'bottom-line' : pageName}]">
+        <!-- 左 -->
+        <div class="left" @click="$emit('onLeftClick')">
+            <!-- 默认状态 -->
+            <img v-if="isShowBack" src="@img/back.svg" alt="">
+            <!-- 具名插槽 -->
+            <slot name="nav-left"></slot>
+        </div>
+        <!-- 中 -->
+        <div class="center">
+            <!-- 默认状态 -->
+            <span class="page-title" v-if="pageName">{{pageName}}</span>
+            <!-- 具名插槽 -->
+            <slot name="nav-center"></slot>
+        </div>
+        <!-- 右 -->
+        <div class="right">
+            <!-- 具名插槽 -->
+            <slot name="nav-right"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        /**
+         * 页面标题的名称
+         */
+        pageName: {
+            type: String,
+            default: ''
+        },
+        /**
+         * 是否展示后退按钮
+         */
+        isShowBack: {
+            type: Boolean,
+            default: true
+        },
+        /**
+         * navBar 样式
+         */
+        navBarStyle: {
+            type: Object,
+            default: function () {
+                return {
+                    backgroundColor: 'white'
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@css/style.scss';
+    .nav-bar {
+        width: 100%;
+        height: px2rem(44);
+        line-height: px2rem(44);
+        display: flex;
+        justify-content: space-between;
+        // 适配沉浸式手机 statusBar
+        padding-top: $statusBarHeight;
+
+        .left, .right {
+            display: flex;
+            height: 100%;
+            width: px2rem(26);
+            padding: 0 $marginSize;
+
+            img {
+                width: 100%;
+                align-self: center;
+            }
+        }
+
+        .center {
+            display: flex;
+            height: 100%;
+            flex-grow: 1;
+
+            .page-title {
+                font-size: $titleSize;
+                align-self: center;
+                margin: 0 auto;
+            }
+        }
+    }
+
+    .bottom-line {
+        border-bottom: 1px solid $lineColor;
+    }
+</style>
+
